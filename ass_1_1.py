2f9f3df86bc7151046fb24dd99e6ff2e8acd26ba
Ronquoris <ronronit1514@gmail.com>
2025-12-30 00:50:00 +0530

    Task_1 and Task_2

---
+++ ass_1_1.py
@@ -0,0 +1,4 @@
+first = str(input("Enter your first name: "))
+last = str(input("Enter your last name: "))
+
+print("Hello,",first,last+"!","Welcome to the Python program.")
---
+++ ass_1_2.py
@@ -0,0 +1,7 @@
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+
+print(f"Addition {num1+num2}")
+print(f"Subtraction {num1-num2}")
+print(f"Multiplication {num1*num2}")
+print(f"Division {round(num1/num2,2)}")
